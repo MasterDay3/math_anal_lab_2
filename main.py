f230d91e71c19e86c01346dce5fc0eb1b8b4989c
dymp1q <dmytrivmaryan2008@gmail.com>
2025-12-01 18:27:40 +0200

    -comments

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sympy import diff, cos, sin, pi, evalf, Symbol, lambdify, integrate
 from scipy import integrate as sp_integrate
 import matplotlib.pyplot as plt
 import time
-#fghh
+
 def riemann_method(func, a: float, b: float, n: int) -> float:
     """
     [a, b] - interval
@@ -11,7 +11,6 @@ def riemann_method(func, a: float, b: float, n: int) -> float:
     func = cos(x), for example, where x = Symbol("x")
     Must return the approximate value of the integral of 'func' on [a, b]
     """
-    #Тут має бути ваш код
     pass
 
 def trapezoidal_method(func, a: float, b: float, n: int) -> float:
@@ -21,32 +20,32 @@ def trapezoidal_method(func, a: float, b: float, n: int) -> float:
     func = 1/x, for example, where x = Symbol("x")
     Must return the approximate value of the integral of 'func' on [a, b]
     """
-    #Тут має бути ваш код
+
     pass
+
 def easy_sci_py(func, a: float, b: float) -> float:
     """
     Computes the integral using SciPy's quad() method
     func = tg(x), for example, where x = Symbol("x")
     Must return highly accurate approximation of the integral
     """
-    #Тут має бути ваш код
+
     pass
 
 def compare_accuracy(func, a: float, b: float, n: int):
     """
     Compares the real integral value and the one received from two upper functions.
     """
-    #Тут має бути ваш код
     pass
 
 def compare_time(func, a, b, n_values):
-  """ Compare speed of calculating by Rimman and Trapezoid"""
-  #Тут має бути ваш код
+  """ 
+  Compare speed of calculating by Rimman and Trapezoid
+  """
   pass
 
 def get_plot_of_error(func, a: float, b: float):
     """
     Consider different n to see the error decrease, then plot the result
     """
-    #Тут має бути ваш код
     pass
