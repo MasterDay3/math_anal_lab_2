baeac634cb3c7b84d878d20e6deca36f06423359
dymp1q <dmytrivmaryan2008@gmail.com>
2025-12-01 18:13:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import numpy as np
+from sympy import diff, cos, sin, pi, evalf, Symbol, lambdify, integrate
+from scipy import integrate as sp_integrate
+import matplotlib.pyplot as plt
+import time
+
 
