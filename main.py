0d0283748d4f841d63c8d6946cc6da5414be34ce
dymp1q <dmytrivmaryan2008@gmail.com>
2025-12-01 18:15:16 +0200

    Implement numerical integration methods and comparisons
    Added functions for numerical integration methods including Riemann, trapezoidal, and SciPy's quad. Also included functions for comparing accuracy, time, and plotting error.

--- main.py
+++ main.py
@@ -4,4 +4,49 @@ from scipy import integrate as sp_integrate
 import matplotlib.pyplot as plt
 import time
 
+def riemann_method(func, a: float, b: float, n: int) -> float:
+    """
+    [a, b] - interval
+    n - number of subintervals in the partition. points in them are chosen randomly
+    func = cos(x), for example, where x = Symbol("x")
+    Must return the approximate value of the integral of 'func' on [a, b]
+    """
+    #Тут має бути ваш код
+    pass
 
+def trapezoidal_method(func, a: float, b: float, n: int) -> float:
+    """
+    [a, b] - interval
+    n - number of subintervals in the partition (all should be the same size)
+    func = 1/x, for example, where x = Symbol("x")
+    Must return the approximate value of the integral of 'func' on [a, b]
+    """
+    #Тут має бути ваш код
+    pass
+def easy_sci_py(func, a: float, b: float) -> float:
+    """
+    Computes the integral using SciPy's quad() method
+    func = tg(x), for example, where x = Symbol("x")
+    Must return highly accurate approximation of the integral
+    """
+    #Тут має бути ваш код
+    pass
+
+def compare_accuracy(func, a: float, b: float, n: int):
+    """
+    Compares the real integral value and the one received from two upper functions.
+    """
+    #Тут має бути ваш код
+    pass
+
+def compare_time(func, a, b, n_values):
+  """ Compare speed of calculating by Rimman and Trapezoid"""
+  #Тут має бути ваш код
+  pass
+
+def get_plot_of_error(func, a: float, b: float):
+    """
+    Consider different n to see the error decrease, then plot the result
+    """
+    #Тут має бути ваш код
+    pass
