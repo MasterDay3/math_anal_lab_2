0f536514cedefa017a5c082692f11ea1e3a647d7
dymp1q <dmytrivmaryan2008@gmail.com>
2025-12-01 18:16:38 +0200

    111

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sympy import diff, cos, sin, pi, evalf, Symbol, lambdify, integrate
 from scipy import integrate as sp_integrate
 import matplotlib.pyplot as plt
 import time
-
+#fghh
 def riemann_method(func, a: float, b: float, n: int) -> float:
     """
     [a, b] - interval
